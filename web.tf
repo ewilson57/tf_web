1716cd8360d143962f0c6736a99c48b54f0f06f5
Earl Wilson <ewilson57@gmail.com>
2021-01-02 10:27:58 -0500

    Changed public IP to static

--- web.tf
+++ web.tf
@@ -48,7 +48,7 @@ resource "azurerm_public_ip" "web-lb" {
   name                = "${var.prefix}-lb-publicip"
   location            = azurerm_resource_group.web.location
   resource_group_name = azurerm_resource_group.web.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_lb" "web" {
