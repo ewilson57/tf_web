a9229eeda3a7e73f87d67dcfa7ecd684ce7e86df
Earl Wilson <ewilson57@gmail.com>
2021-01-02 21:42:56 -0500

    Fixed lb rule

--- web.tf
+++ web.tf
@@ -81,6 +81,8 @@ resource "azurerm_lb_rule" "web" {
   frontend_port                  = 80
   backend_port                   = 80
   frontend_ip_configuration_name = "LoadBalancerFrontEnd"
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.web.id
+  probe_id                       = azurerm_lb_probe.web.id
 }
 
 resource "azurerm_lb_probe" "web" {
